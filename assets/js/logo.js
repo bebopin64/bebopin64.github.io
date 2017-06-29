65539b4c7e198033aa6b31238e0dffdfb694fb26
William Garlick <bokeeliawill@gmail.com>
2017-06-28 23:40:19 -0500

    more optimization

--- assets/js/logo.js
+++ assets/js/logo.js
@@ -1,8 +1,8 @@
 $("body").on('mousemove', function(e) {
 	var xRel = e.pageX;
 	var yRel = e.pageY;
-	var yAngle = ((xRel / window.innerWidth) - .5) * 5;
-	var xAngle = -((yRel / window.innerHeight) - .5) * 5;
+	var yAngle = ((xRel / window.innerWidth) - .5) * 3;
+	var xAngle = -((yRel / window.innerHeight) - .5) * 3;
 	updateView(xAngle, yAngle);
 })
 
@@ -10,5 +10,9 @@ function updateView(xAngle, yAngle) {
 	var transform = "rotateX(" + xAngle + "deg) rotateY(" + yAngle + "deg) translateX("+ (-yAngle*3) +"px) translateY("+ (xAngle*5) +"px)";
 	$(".logo-cont").css({
 		'transform': transform
-	})
+	});
+	var bgtransform = "translateX(" + yAngle*.5 + "vw) translateY(" + -xAngle*.5 + "vh) scale(1.1)";
+	$(".page-background").css({
+		'transform': bgtransform
+	});
 }
